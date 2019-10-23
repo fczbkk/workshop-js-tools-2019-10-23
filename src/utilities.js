2de8a05773c7177e12a79363fe15fa34f3f32f4a
Riki Fridrich <riki@fczbkk.com>
2019-10-23 16:34:45 +0200

    feat: add inline documentation

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,19 @@
+/**
+ * Adds two number and returns their sum.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 export function add (a, b) {
   return a + b
 }
 
+/**
+ * Multiplies two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 export function multiply (a, b) {
   return a * b
 }
